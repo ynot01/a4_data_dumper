04c2a9d3c0432b390c0529a9c9587ae86ae51e27
ynot01 <ynot000001@gmail.com>
2025-08-05 19:59:51 -0400

    Convert indentation to tabs

--- DataDumper.cs
+++ DataDumper.cs
@@ -13,158 +13,158 @@ namespace data_dumper
 		void Update()
 		{
 			Scene uiScene = SceneManager.GetSceneByName("UserInterface");
-            if (uiScene.loadingState == Scene.LoadingState.Loaded)
-            {
-                string Path = Application.persistentDataPath + "/item_data.json";
-                string myData = "";
-                myData += @"{
+			if (uiScene.loadingState == Scene.LoadingState.Loaded)
+			{
+				string Path = Application.persistentDataPath + "/item_data.json";
+				string myData = "";
+				myData += @"{
 				""WeaponData"": {
 				";
 
-                Entities.Items.WeaponData[] objs = Resources.FindObjectsOfTypeAll<Entities.Items.WeaponData>();
+				Entities.Items.WeaponData[] objs = Resources.FindObjectsOfTypeAll<Entities.Items.WeaponData>();
 
-                foreach (var go in objs)
-                {
-                    myData += "	\"" + go.name + "\":";
-                    myData += JsonUtility.ToJson(go);
-                    myData += ",\n";
-                }
-                myData += "	\"Null\":{}";
+				foreach (var go in objs)
+				{
+					myData += "	\"" + go.name + "\":";
+					myData += JsonUtility.ToJson(go);
+					myData += ",\n";
+				}
+				myData += "	\"Null\":{}";
 
-                myData += "\n},\n";
+				myData += "\n},\n";
 
 
 
-                myData += @"
+				myData += @"
 				""TagsWeapon"": {
 				";
 
 
-                int tag = 0;
-                foreach (var go in Enum.GetValues(typeof(Entities.Shared.TagsWeapon)).Cast<Entities.Shared.TagsWeapon>())
-                {
-                    myData += "	\"" + tag + "\":\"" + go + "\"";
-                    myData += ",\n";
-                    tag++;
-                }
-                myData += "	\"-1\":\"Null\"";
-                myData += "\n},\n";
+				int tag = 0;
+				foreach (var go in Enum.GetValues(typeof(Entities.Shared.TagsWeapon)).Cast<Entities.Shared.TagsWeapon>())
+				{
+					myData += "	\"" + tag + "\":\"" + go + "\"";
+					myData += ",\n";
+					tag++;
+				}
+				myData += "	\"-1\":\"Null\"";
+				myData += "\n},\n";
 
 
 
-                myData += @"
+				myData += @"
 				""FiringType"": {
 				";
 
 
-                int fir = 0;
-                foreach (var go in Enum.GetValues(typeof(Entities.Items.WeaponData.FiringType)).Cast<Entities.Items.WeaponData.FiringType>())
-                {
-                    myData += "	\"" + fir + "\":\"" + go + "\"";
-                    myData += ",\n";
-                    fir++;
-                }
-                myData += "	\"-1\":\"Null\"";
-                myData += "\n},\n";
+				int fir = 0;
+				foreach (var go in Enum.GetValues(typeof(Entities.Items.WeaponData.FiringType)).Cast<Entities.Items.WeaponData.FiringType>())
+				{
+					myData += "	\"" + fir + "\":\"" + go + "\"";
+					myData += ",\n";
+					fir++;
+				}
+				myData += "	\"-1\":\"Null\"";
+				myData += "\n},\n";
 
 
 
-                myData += @"
+				myData += @"
 				""Attributes"": {
 				";
 
 
-                int att = 0;
-                foreach (var go in Enum.GetValues(typeof(Entities.Player.PlayerAttributes.Attributes)).Cast<Entities.Player.PlayerAttributes.Attributes>())
-                {
-                    myData += "	\"" + att + "\":\"" + go + "\"";
-                    myData += ",\n";
-                    att++;
-                }
-                myData += "	\"-1\":\"Null\"";
-                myData += "\n},\n";
+				int att = 0;
+				foreach (var go in Enum.GetValues(typeof(Entities.Player.PlayerAttributes.Attributes)).Cast<Entities.Player.PlayerAttributes.Attributes>())
+				{
+					myData += "	\"" + att + "\":\"" + go + "\"";
+					myData += ",\n";
+					att++;
+				}
+				myData += "	\"-1\":\"Null\"";
+				myData += "\n},\n";
 
 
 
-                myData += @"
+				myData += @"
 				""Consumable"": {
 				";
 
-                Entities.Items.Consumable[] objsb = Resources.FindObjectsOfTypeAll<Entities.Items.Consumable>();
+				Entities.Items.Consumable[] objsb = Resources.FindObjectsOfTypeAll<Entities.Items.Consumable>();
 
-                foreach (var go in objsb)
-                {
-                    myData += "	\"" + go.name + "\":";
-                    myData += JsonUtility.ToJson(go);
-                    myData += ",\n";
-                }
-                myData += "	\"Null\":{}";
-                myData += "\n},\n";
+				foreach (var go in objsb)
+				{
+					myData += "	\"" + go.name + "\":";
+					myData += JsonUtility.ToJson(go);
+					myData += ",\n";
+				}
+				myData += "	\"Null\":{}";
+				myData += "\n},\n";
 
 
 
-                myData += @"
+				myData += @"
 				""EntityData"": {
 				";
 
 
-                Entities.Shared.EntityData[] objsa = Resources.FindObjectsOfTypeAll<Entities.Shared.EntityData>();
+				Entities.Shared.EntityData[] objsa = Resources.FindObjectsOfTypeAll<Entities.Shared.EntityData>();
 
-                foreach (var go in objsa)
-                {
-                    myData += "	\"" + go.name + "\":";
-                    myData += JsonUtility.ToJson(go);
-                    myData += ",\n";
-                }
-                myData += "	\"Null\":{}";
-                myData += "\n},\n";
+				foreach (var go in objsa)
+				{
+					myData += "	\"" + go.name + "\":";
+					myData += JsonUtility.ToJson(go);
+					myData += ",\n";
+				}
+				myData += "	\"Null\":{}";
+				myData += "\n},\n";
 
 
 
-                myData += @"
+				myData += @"
 				""Fate"": {
 				";
 
-                Entities.Player.Fate[] objsd = Resources.FindObjectsOfTypeAll<Entities.Player.Fate>();
+				Entities.Player.Fate[] objsd = Resources.FindObjectsOfTypeAll<Entities.Player.Fate>();
 
-                foreach (var go in objsd)
-                {
-                    myData += "	\"" + go.name + "\":";
-                    myData += JsonUtility.ToJson(go);
-                    myData += ",\n";
-                }
-                myData += "	\"Null\":{}";
-                myData += "\n},\n";
+				foreach (var go in objsd)
+				{
+					myData += "	\"" + go.name + "\":";
+					myData += JsonUtility.ToJson(go);
+					myData += ",\n";
+				}
+				myData += "	\"Null\":{}";
+				myData += "\n},\n";
 
 
 
-                myData += @"
+				myData += @"
 				""InstanceIDs"": {
 				";
 
-                foreach (var go in objsa)
-                {
-                    myData += "	\"" + go.GetInstanceID() + "\":\"" + go.name + "\"";
-                    myData += ",\n";
-                }
+				foreach (var go in objsa)
+				{
+					myData += "	\"" + go.GetInstanceID() + "\":\"" + go.name + "\"";
+					myData += ",\n";
+				}
 
-                foreach (var go in objsd)
-                {
-                    myData += "	\"" + go.GetInstanceID() + "\":\"" + go.name + "\"";
-                    myData += ",\n";
-                }
-                myData += "	\"0\":\"Null\"";
+				foreach (var go in objsd)
+				{
+					myData += "	\"" + go.GetInstanceID() + "\":\"" + go.name + "\"";
+					myData += ",\n";
+				}
+				myData += "	\"0\":\"Null\"";
 
-                myData += "\n}";
+				myData += "\n}";
 
-                myData += "\n}";
+				myData += "\n}";
 
-                var des = JsonConvert.DeserializeObject(myData);
-                File.WriteAllText(Path, JsonConvert.SerializeObject(des, Formatting.Indented));
+				var des = JsonConvert.DeserializeObject(myData);
+				File.WriteAllText(Path, JsonConvert.SerializeObject(des, Formatting.Indented));
 
-                Application.Quit();
-            }
-        }
+				Application.Quit();
+			}
+		}
 	}
 
 	[BepInPlugin("data_dumper", "Data Dumper", "0.0.1")]

8b9e46b6c23184d74a001e11190173d6f0392266
ynot01 <ynot000001@gmail.com>
2025-08-05 19:56:40 -0400

    initial commit

---
+++ DataDumper.cs
@@ -0,0 +1,184 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using BepInEx.Unity.IL2CPP;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Newtonsoft.Json;
+
+namespace data_dumper
+{
+
+	public class DataDumper_Component : MonoBehaviour 
+	{
+		void Update()
+		{
+			Scene uiScene = SceneManager.GetSceneByName("UserInterface");
+            if (uiScene.loadingState == Scene.LoadingState.Loaded)
+            {
+                string Path = Application.persistentDataPath + "/item_data.json";
+                string myData = "";
+                myData += @"{
+				""WeaponData"": {
+				";
+
+                Entities.Items.WeaponData[] objs = Resources.FindObjectsOfTypeAll<Entities.Items.WeaponData>();
+
+                foreach (var go in objs)
+                {
+                    myData += "	\"" + go.name + "\":";
+                    myData += JsonUtility.ToJson(go);
+                    myData += ",\n";
+                }
+                myData += "	\"Null\":{}";
+
+                myData += "\n},\n";
+
+
+
+                myData += @"
+				""TagsWeapon"": {
+				";
+
+
+                int tag = 0;
+                foreach (var go in Enum.GetValues(typeof(Entities.Shared.TagsWeapon)).Cast<Entities.Shared.TagsWeapon>())
+                {
+                    myData += "	\"" + tag + "\":\"" + go + "\"";
+                    myData += ",\n";
+                    tag++;
+                }
+                myData += "	\"-1\":\"Null\"";
+                myData += "\n},\n";
+
+
+
+                myData += @"
+				""FiringType"": {
+				";
+
+
+                int fir = 0;
+                foreach (var go in Enum.GetValues(typeof(Entities.Items.WeaponData.FiringType)).Cast<Entities.Items.WeaponData.FiringType>())
+                {
+                    myData += "	\"" + fir + "\":\"" + go + "\"";
+                    myData += ",\n";
+                    fir++;
+                }
+                myData += "	\"-1\":\"Null\"";
+                myData += "\n},\n";
+
+
+
+                myData += @"
+				""Attributes"": {
+				";
+
+
+                int att = 0;
+                foreach (var go in Enum.GetValues(typeof(Entities.Player.PlayerAttributes.Attributes)).Cast<Entities.Player.PlayerAttributes.Attributes>())
+                {
+                    myData += "	\"" + att + "\":\"" + go + "\"";
+                    myData += ",\n";
+                    att++;
+                }
+                myData += "	\"-1\":\"Null\"";
+                myData += "\n},\n";
+
+
+
+                myData += @"
+				""Consumable"": {
+				";
+
+                Entities.Items.Consumable[] objsb = Resources.FindObjectsOfTypeAll<Entities.Items.Consumable>();
+
+                foreach (var go in objsb)
+                {
+                    myData += "	\"" + go.name + "\":";
+                    myData += JsonUtility.ToJson(go);
+                    myData += ",\n";
+                }
+                myData += "	\"Null\":{}";
+                myData += "\n},\n";
+
+
+
+                myData += @"
+				""EntityData"": {
+				";
+
+
+                Entities.Shared.EntityData[] objsa = Resources.FindObjectsOfTypeAll<Entities.Shared.EntityData>();
+
+                foreach (var go in objsa)
+                {
+                    myData += "	\"" + go.name + "\":";
+                    myData += JsonUtility.ToJson(go);
+                    myData += ",\n";
+                }
+                myData += "	\"Null\":{}";
+                myData += "\n},\n";
+
+
+
+                myData += @"
+				""Fate"": {
+				";
+
+                Entities.Player.Fate[] objsd = Resources.FindObjectsOfTypeAll<Entities.Player.Fate>();
+
+                foreach (var go in objsd)
+                {
+                    myData += "	\"" + go.name + "\":";
+                    myData += JsonUtility.ToJson(go);
+                    myData += ",\n";
+                }
+                myData += "	\"Null\":{}";
+                myData += "\n},\n";
+
+
+
+                myData += @"
+				""InstanceIDs"": {
+				";
+
+                foreach (var go in objsa)
+                {
+                    myData += "	\"" + go.GetInstanceID() + "\":\"" + go.name + "\"";
+                    myData += ",\n";
+                }
+
+                foreach (var go in objsd)
+                {
+                    myData += "	\"" + go.GetInstanceID() + "\":\"" + go.name + "\"";
+                    myData += ",\n";
+                }
+                myData += "	\"0\":\"Null\"";
+
+                myData += "\n}";
+
+                myData += "\n}";
+
+                var des = JsonConvert.DeserializeObject(myData);
+                File.WriteAllText(Path, JsonConvert.SerializeObject(des, Formatting.Indented));
+
+                Application.Quit();
+            }
+        }
+	}
+
+	[BepInPlugin("data_dumper", "Data Dumper", "0.0.1")]
+	public class DataDumper : BasePlugin
+	{
+		internal static new ManualLogSource Log;
+
+		public override void Load()
+		{
+			Log = base.Log;
+
+			AddComponent<DataDumper_Component>();
+
+			Log.LogInfo($"Plugin data_dumper loaded successfully.");
+		}
+	}
+}
---
+++ README.md
@@ -0,0 +1,16 @@
+# ANEURISM IV Data Dumper
+
+This plugin is meant primarily for developers to dump data that is useful to the wiki, for wiki data automation
+
+Once the UI scene is loaded, it will gather game object data and export it to `AppData\LocalLow\Vellocet\ANEURISM IV\item_data.json`
+
+The data exported, at the time of writing, includes:
+- Weapon data (firerate, recoil, damage, damage type, ammo count, ammo types)
+- Consumable data (effects, attributes, exceptions like sus meat by scum)
+- Entity data (weight, contraband, inventoriable, soulbound)
+- Instance IDs (used in some places like ammo or consumable fate exceptions)
+- Fates (anamnecyte cost, karma req, loadout, health, speed)
+- Enums
+  - Attributes (for consumables)
+  - TagsWeapon (damage type)
+  - FiringType (single or full auto)
